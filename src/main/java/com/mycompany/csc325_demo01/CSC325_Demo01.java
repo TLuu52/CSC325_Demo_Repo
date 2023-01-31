8f62be5d544d3973e5ae060be03cdacfcf0f6059
TLuu52 <luuthuan817@gmail.com>
2023-01-30 20:10:26 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/csc325_demo01/CSC325_Demo01.java
@@ -0,0 +1,15 @@
+package com.mycompany.csc325_demo01;
+
+/**
+ *
+ * @author Luuth
+ */
+public class CSC325_Demo01 {
+    public static void main(String[] args) {
+        System.out.println("Thuan Luu \n\n\n");
+        
+        System.out.println("Thuan Luu \n\n\n");
+        System.out.println("Thuan Luu \n\n\n");
+        System.out.println("Thuan Luu \n\n\n");
+    }
+}
